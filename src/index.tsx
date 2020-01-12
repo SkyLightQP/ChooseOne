e16d003c3037beda902a39430771cccde46874fc
SkyLightQP <combbm@naver.com>
2020-01-12 23:37:36 +0900

    feat: add color palette and apply it

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'react-app-polyfill/ie9';
 import 'normalize.css/normalize.css';
+import './css/color.css';
 import Home from './pages/Home';
-
 
 ReactDOM.render(<Home />, document.getElementById('root'));
