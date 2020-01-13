e342b4af67cad9def8b28f2b664c8a0fa797162d
SkyLightQP <combbm@naver.com>
2020-01-13 23:38:09 +0900

    feat: add SubTitle component according to user

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
-import styled from "styled-components";
+import styled from 'styled-components';
 import Title from '../atomics/Title';
+import Container from '../utils/Container';
+import { useVisited } from '../hooks/useVisited';
 import SubTitle from '../atomics/SubTitle';
 
 const Header = styled.header`
-    display: flex;
-    justify-content: center;
-    margin-top: 2rem;
+    margin-top: 4rem;
+    margin-left: 1rem;
 `;
 
 const Home: React.FC = () => {
+    const visited = useVisited();
+
+    const NewbieSubTitle = () => <SubTitle>둘 중에 고민될 때 이용해보세요!</SubTitle>;
+    const BasicSubTitle = () => <SubTitle>골라봐요!</SubTitle>;
+
     return (
         <>
             <Header>
-                <div>
-                    <Title>Choose One</Title>
-                    <SubTitle>골라봐요!</SubTitle>
-                </div>
+                <Container>
+                    <Title>
+                        Choose <i>One</i>
+                    </Title>
+                    {visited === 'true' ? <BasicSubTitle /> : <NewbieSubTitle />}
+                </Container>
             </Header>
         </>
     );
