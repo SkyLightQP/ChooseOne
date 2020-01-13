de91da1fb5f2e4e26ca1aba3d1ed3dfd88791056
SkyLightQP <combbm@naver.com>
2020-01-13 23:33:15 +0900

    fix: change font size to SubTitle.tsx atomic

--- src/atomics/SubTitle/index.tsx
+++ src/atomics/SubTitle/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const SubTitleStyle = styled.p`
-    font-size: 2.4rem;
+    font-size: 2rem;
     font-weight: 700;
     margin: 0.2rem 0;
 `;
