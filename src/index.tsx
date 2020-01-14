d25e987d68447ae05bae5c5ad6f033fefd9c303e
SkyLightQP <combbm@naver.com>
2020-01-14 21:43:00 +0900

    chore: remove react-app-polyfill dependency

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'react-app-polyfill/ie9';
 import 'normalize.css/normalize.css';
 import './css/color.css';
 import Home from './pages/Home';
