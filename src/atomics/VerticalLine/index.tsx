1c465870a29da793e4dafccbb22df7c982e5df20
SkyLightQP <combbm@naver.com>
2020-01-14 19:06:41 +0900

    feat: add VerticalLine component

---
+++ src/atomics/VerticalLine/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const LineStyle = styled.div`
+    border-left: 0.2rem solid #ced6e0;
+    border-radius: 10px;
+    height: 10rem;
+    margin: 0 4rem;
+
+    .card:hover & {
+        border-left: 0.2rem solid #747d8c;
+    }
+`;
+
+const VerticalLine: React.FC = () => {
+    return <LineStyle />;
+};
+
+export default VerticalLine;
