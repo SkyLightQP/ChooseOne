87acd474072fab487ab7794bce5cc4efc4775b85
SkyLightQP <combbm@naver.com>
2020-01-17 18:22:55 +0900

    feat: add useData() hooks

---
+++ src/hooks/useData.tsx
@@ -0,0 +1,51 @@
+import React, { createContext, useContext, useState } from 'react';
+
+interface DataStructure {
+    readonly first: any;
+    readonly second: any;
+}
+
+interface DataContext {
+    readonly put: (first: any, second: any) => void;
+    readonly remove: (id: string) => void;
+    readonly get: () => DataStructure[];
+}
+
+const context = createContext<DataContext>({} as DataContext);
+
+export const DataProvider: React.FC = ({ children }) => {
+    const [data, setData] = useState<DataStructure[]>([]);
+
+    const DataManager: DataContext = {
+        put(first: any, second: any) {
+            if (localStorage.getItem('data') === null) {
+                localStorage.setItem('data', JSON.stringify([]));
+            }
+
+            const local = JSON.parse(localStorage.getItem('data')!!);
+            const obj: DataStructure = {
+                first,
+                second
+            };
+
+            local.push(obj);
+
+            setData(local);
+            localStorage.setItem('data', JSON.stringify(local));
+        },
+        remove() {
+            const local = JSON.parse(localStorage.getItem('data')!!);
+            local.pop();
+
+            setData(local);
+            localStorage.setItem('data', JSON.stringify(local));
+        },
+        get(): DataStructure[] {
+            return data;
+        }
+    };
+
+    return <context.Provider value={DataManager}>{children}</context.Provider>;
+};
+
+export const useData = () => useContext(context);
--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,14 @@ import 'normalize.css/normalize.css';
 import './css/color.css';
 import Home from './pages/Home';
 import { VisitedProvider } from './hooks/useVisited';
+import { DataProvider } from './hooks/useData';
 
 const Index = () => (
-    <VisitedProvider>
-        <Home />
-    </VisitedProvider>
+    <DataProvider>
+        <VisitedProvider>
+            <Home />
+        </VisitedProvider>
+    </DataProvider>
 );
 
 ReactDOM.render(<Index />, document.getElementById('root'));
