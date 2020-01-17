fd7a6cf089fc5bfbb96729dbaddeb2fddcf98b73
SkyLightQP <combbm@naver.com>
2020-01-17 18:21:52 +0900

    fix: change width unit in VerticalLine component

--- src/atomics/VerticalLine/index.tsx
+++ src/atomics/VerticalLine/index.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 
 const LineStyle = styled.div`
-    border-left: 0.1rem solid #ced6e0;
+    border-left: 1px solid #ced6e0;
     border-radius: 10px;
     height: 10rem;
 
 
     .card:hover & {
-        border-left: 0.1rem solid #a4b0be;
+        border-left: 1px solid #a4b0be;
     }
 `;
 
